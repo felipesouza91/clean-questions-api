100a513b9b356a16b07830ced4b02a968a3adbc3
Felipe Souza <felipedb91@gmail.com>
2020-07-25 12:37:00 -0300

    feat: ensure SignUpController calls AddAccount with corrects values

--- src/presentation/controller/SignUpController.ts
+++ src/presentation/controller/SignUpController.ts
@@ -3,30 +3,36 @@ import { MissingParamError, InvalidParamError } from '../erros'
 import { badRequest, serverError } from '../helpers/HttpHelper'
 
 import { ServerError } from '../erros/ServerError'
+import { AddAccount } from '../../domain/usecases/AddAccount'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFilds = ['email', 'name', 'password', 'passwordConfirmation']
-      const { body } = httpRequest
+      const { name, password, email,passwordConfirmation } = httpRequest.body
+
       for (const field of requiredFilds) {
-        if (!body[field]) {
+        if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (body.password !== body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isValid = this.emailValidator.isValid(body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccount.add({ name, password, email })
     } catch (error) {
+      console.error(error)
       return serverError(new ServerError())
     }
   }

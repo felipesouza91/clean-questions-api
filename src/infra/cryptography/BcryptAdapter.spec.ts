45e88e651d7dfb92144a65c22581b224cee7b811
Felipe Souza <felipedb91@gmail.com>
2020-07-26 21:52:58 -0300

    refactor: on BcryptAdapter move sut creation to a helper method

--- src/infra/cryptography/BcryptAdapter.spec.ts
+++ src/infra/cryptography/BcryptAdapter.spec.ts
@@ -1,24 +1,34 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './BcryptAdapter'
 
+const salt = 12
+
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hashed_value'))
   }
 }))
 
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
+    const hashedValud = await sut.encrypt('any_value')
+    expect(hashedValud).toBe('hashed_value')
+  })
+
+  test('should return a hash on success', async () => {
+    const sut = makeSut()
     const hashedValud = await sut.encrypt('any_value')
     expect(hashedValud).toBe('hashed_value')
   })

59d9850ce54e3ba9662f65b70fe23f9498dbd432
Felipe Souza <felipedb91@gmail.com>
2020-08-14 22:03:59 -0300

    refactor: configure jest files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/*.protocols.ts',
+    '!<rootDir>/src/main/adapter/*.ts',
     '!**/protocols/**',
     '!<rootDir>/src/main/config/env.ts',
     '!<rootDir>/src/main/server.ts'

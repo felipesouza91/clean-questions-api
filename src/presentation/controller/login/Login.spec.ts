94847359cb04d6e7baf171e48c434e760e6d7e1a
Felipe Souza <felipedb91@gmail.com>
2020-08-14 23:57:32 -0300

    test: ensure login controller calls EmailValidator with correct email

--- src/presentation/controller/login/Login.spec.ts
+++ src/presentation/controller/login/Login.spec.ts
@@ -2,14 +2,26 @@ import { LoginController } from './Login'
 import { badRequest } from '../../helpers/HttpHelper'
 import { MissingParamError } from '../../erros'
 import { Controller } from '../../protocols'
+import { EmailValidator } from '../signup/SignUp.protocols'
 
 interface SutTypes {
   sut: Controller
+  emailValidator: EmailValidator
+}
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorMock implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorMock()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
-  return { sut }
+  const emailValidator = makeEmailValidator()
+  const sut = new LoginController(emailValidator)
+  return { sut, emailValidator }
 }
 
 describe('Login Controller', () => {
@@ -32,5 +44,27 @@ describe('Login Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+  test('should return 400 if no email is password', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+  test('should call EmailValidator with correct email', async () => {
+    const { sut, emailValidator } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidator, 'isValid')
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })

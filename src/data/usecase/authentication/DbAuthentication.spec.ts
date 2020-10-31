de7f5fd9a623f393ab6b5803086419ead3a3595a
Felipe Souza <felipedb91@gmail.com>
2020-10-31 14:05:43 -0300

    feat: ensure DbAuthentication calls hash compare with correct values

--- src/data/usecase/authentication/DbAuthentication.spec.ts
+++ src/data/usecase/authentication/DbAuthentication.spec.ts
@@ -1,6 +1,9 @@
-
 import { AccountModel } from '../../../domain/models/Account'
-import { Authentication, AuthenticationModel } from '../../../domain/usecases/Authentication'
+import {
+  Authentication,
+  AuthenticationModel
+} from '../../../domain/usecases/Authentication'
+import { IHashCompare } from '../../protocols/cryptography/IHashCompare'
 import { ILoadAccountByEmailRepository } from '../../protocols/db/ILoadAccountByEmailRepository'
 
 import { DbAuthentication } from './DbAuthentication'
@@ -8,13 +11,14 @@ import { DbAuthentication } from './DbAuthentication'
 interface SutTypes {
   sut: Authentication
   loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository
+  hashCompareStub: IHashCompare
 }
 
 const makeFakeAccount = (): AccountModel => ({
   id: '1',
   email: 'any_email@mail.com',
   name: 'Any Name',
-  password: 'any_password'
+  password: 'hashed_password'
 })
 
 const makeFakeAuthentication = (): AuthenticationModel => ({
@@ -23,20 +27,36 @@ const makeFakeAuthentication = (): AuthenticationModel => ({
 })
 
 const makeLoadAccountByEmailReposiotyStub = (): ILoadAccountByEmailRepository => {
-  class LoadAccountByEmailRepositoryStub implements ILoadAccountByEmailRepository {
+  class LoadAccountByEmailRepositoryStub
+  implements ILoadAccountByEmailRepository {
     async load (email: string): Promise<AccountModel> {
       const account = makeFakeAccount()
-      return await new Promise(resolve => resolve(account))
+      return await new Promise((resolve) => resolve(account))
     }
   }
   return new LoadAccountByEmailRepositoryStub()
 }
 
+const makeCompareHash = (): IHashCompare => {
+  class HashComapeStub implements IHashCompare {
+    async compare (value: string, hashed: string): Promise<boolean> {
+      return true
+    }
+  }
+  return new HashComapeStub()
+}
+
 const makeSut = (): SutTypes => {
   const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailReposiotyStub()
-  const sut = new DbAuthentication({ loadAccountByEmailRepository: loadAccountByEmailRepositoryStub })
+  const hashCompareStub = makeCompareHash()
+  const sut = new DbAuthentication({
+    loadAccountByEmailRepository: loadAccountByEmailRepositoryStub,
+    hashCompare: hashCompareStub
+  })
   return {
-    sut, loadAccountByEmailRepositoryStub
+    sut,
+    loadAccountByEmailRepositoryStub,
+    hashCompareStub
   }
 }
 
@@ -50,17 +70,28 @@ describe('DbAuthentication UseCase', () => {
 
   test('shoud throw if LoadAccountByEmailRepository throws', async () => {
     const { loadAccountByEmailRepositoryStub, sut } = makeSut()
-    jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
-      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
     const response = sut.auth(makeFakeAuthentication())
     await expect(response).rejects.toThrow()
   })
 
   test('should return null if LoadAccountByEmailRepository returns null', async () => {
-    const { sut , loadAccountByEmailRepositoryStub } = makeSut()
-    jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
       .mockReturnValueOnce(null)
     const response = await sut.auth(makeFakeAuthentication())
     expect(response).toBeNull()
+  })
+
+  test('should call HashCompare with correct values', async () => {
+    const { hashCompareStub, sut } = makeSut()
+    const spyCompare = jest.spyOn(hashCompareStub, 'compare')
+    await sut.auth(makeFakeAuthentication())
+    expect(spyCompare).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
 })

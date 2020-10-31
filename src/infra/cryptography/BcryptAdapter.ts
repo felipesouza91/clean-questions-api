b1a4db61811081f2cb922cd52ec92be88200726a
Felipe Souza <felipedb91@gmail.com>
2020-10-31 12:47:15 -0300

    refactor: move data layer interfaces to folders

--- src/infra/cryptography/BcryptAdapter.ts
+++ src/infra/cryptography/BcryptAdapter.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from '../../data/protocols/Encrypter'
+import { Encrypter } from '../../data/protocols/cryptography/Encrypter'
 import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Encrypter {

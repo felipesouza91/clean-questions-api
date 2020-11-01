be74e63ac48218de790299de9c859358af60ede3
Felipe Souza <felipedb91@gmail.com>
2020-11-01 14:14:48 -0300

    test: ensure BCryptAdapter call bcrypt compare with correct values

--- src/infra/cryptography/BcryptAdapter.spec.ts
+++ src/infra/cryptography/BcryptAdapter.spec.ts
@@ -6,6 +6,9 @@ const salt = 12
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hashed_value'))
+  },
+  async compare (data: any, encrypted: string): Promise<boolean> {
+    return true
   }
 }))
 
@@ -14,14 +17,21 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('should call bcrypt with correct values', async () => {
+  test('should call hash with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('should return a hash on success', async () => {
+  test('should call compare with correct values', async () => {
+    const sut = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'hashed_value')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'hashed_value')
+  })
+
+  test('should return a valid hash on success', async () => {
     const sut = makeSut()
     const hashedValud = await sut.hash('any_value')
     expect(hashedValud).toBe('hashed_value')

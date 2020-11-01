2ea18edf384f2ca890a16f8f967630dcf8d87a5f
Felipe Souza <felipedb91@gmail.com>
2020-10-31 22:25:47 -0300

    refactor: rename interface IEncrypter and IAddAccountRepository

--- src/data/protocols/db/IAddAccountRepository.ts
+++ src/data/protocols/db/IAddAccountRepository.ts
@@ -1,6 +1,6 @@
 import { AddAccountModel } from '../../../domain/usecases/AddAccount'
 import { AccountModel } from '../../../domain/models/Account'
 
-export interface AddAccountRepository {
+export interface IAddAccountRepository {
   add: (accountData: AddAccountModel) => Promise<AccountModel>
 }

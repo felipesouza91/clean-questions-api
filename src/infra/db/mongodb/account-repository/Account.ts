f91cb2253ff3afcd730448c7f38923426f2d5eaf
Felipe Souza <felipedb91@gmail.com>
2020-10-31 22:27:10 -0300

    refactor: rename interface IEncrypter and IAddAccountRepository

--- src/infra/db/mongodb/account-repository/Account.ts
+++ src/infra/db/mongodb/account-repository/Account.ts
@@ -1,8 +1,8 @@
-import { AddAccountRepository } from '../../../../data/protocols/db/IAddAccountRepository'
+import { IAddAccountRepository } from '../../../../data/protocols/db/IAddAccountRepository'
 import { AddAccountModel } from '../../../../domain/usecases/AddAccount'
 import { AccountModel } from '../../../../domain/models/Account'
 import { MongoHelper } from '../helpers/MongoHelper'
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements IAddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)

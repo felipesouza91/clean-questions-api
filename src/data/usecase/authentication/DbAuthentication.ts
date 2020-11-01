a2c8a9454d6ecddb7ac154ae61df5dc8f8b1e34d
Felipe Souza <felipedb91@gmail.com>
2020-10-31 22:09:09 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecase/authentication/DbAuthentication.ts
+++ src/data/usecase/authentication/DbAuthentication.ts
@@ -6,25 +6,30 @@ import { IHashCompare } from '../../protocols/cryptography/IHashCompare'
 import { ITokenGenerator } from '../../protocols/cryptography/ITokenGenerator'
 
 import { ILoadAccountByEmailRepository } from '../../protocols/db/ILoadAccountByEmailRepository'
+import { IUpdateAccessTokenRepository } from '../../protocols/db/IUpdateAccessTokenRepository'
 
 interface DbAuthenticationProps {
   loadAccountByEmailRepository: ILoadAccountByEmailRepository
   hashCompare: IHashCompare
   tokenGenerator: ITokenGenerator
+  updateAccessTokenRepository: IUpdateAccessTokenRepository
 }
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
   private readonly hashCompare: IHashCompare
   private readonly tokenGenerator: ITokenGenerator
+  private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
   constructor ({
     loadAccountByEmailRepository,
     hashCompare,
-    tokenGenerator
+    tokenGenerator,
+    updateAccessTokenRepository
   }: DbAuthenticationProps) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashCompare = hashCompare
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth ({ email, password }: AuthenticationModel): Promise<string> {
@@ -33,6 +38,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashCompare.compare(password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }

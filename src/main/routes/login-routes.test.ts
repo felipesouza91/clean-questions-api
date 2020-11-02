63975e48da1dbb1c70250e51c4502e66a1d27a1e
Felipe Souza <felipedb91@gmail.com>
2020-11-02 15:30:26 -0300

    refactor: rename SignUp-routes with LoginRoutes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper'
 import app from '../config/app'
 
-describe('SingUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,16 +15,17 @@ describe('SingUp Routes', () => {
     const accountsCollection = await MongoHelper.getCollection('accounts')
     await accountsCollection.deleteMany({})
   })
-
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Felipe Souza Santana',
-        email: 'felipedb91@hotmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
-      })
-      .expect(201)
+  describe('POST /signup', () => {
+    test('Should return 201 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Felipe Souza Santana',
+          email: 'felipedb91@hotmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(201)
+    })
   })
 })

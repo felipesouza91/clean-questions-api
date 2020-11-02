71ba0ee4cf5f0b306ea3a0b7fa55dc712c338031
Felipe Souza <felipedb91@gmail.com>
2020-11-02 16:28:15 -0300

    test: ensure login route returns 401 if invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'felipedb91@hotmail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })

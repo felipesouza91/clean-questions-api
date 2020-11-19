23f28d32fd8ffdf48182cfbb3e4597ba3231ffae
Felipe Souza <felipedb91@gmail.com>
2020-11-18 22:35:43 -0300

    test: ensure add survey routes returns 204 if valid accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,10 +1,13 @@
 import { Collection } from 'mongodb'
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper'
 import app from '../config/app'
+import env from '../config/env'
 
 describe('Survey Routes', () => {
   let surveyCollection: Collection
+  let accountCollection: Collection
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,7 +18,9 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return 403 on add survey without accessToken', async () => {
@@ -34,6 +39,34 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+    test('Should return 204 on add survey with accessToken', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        role: 'admin'
+
+      })
+      const id = account.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })

d6421064340765011e90103d2064f828e9f3c9cd
Felipe Souza <felipedb91@gmail.com>
2020-11-23 22:57:53 -0300

    test: ensure DbSaveSurveyResult useCase call SaveSurveyResultRepository with correct values

---
+++ src/data/usecase/save-survey-result/DbSaveSurveyResult.spec.ts
@@ -0,0 +1,55 @@
+import { ISaveSurveyResultRepository, IAddSurveyResultModel, ISurveyResultModel } from './DbSaveSurveyResult.protocols'
+import { DbSaveSurveyResult } from './DbSaveSurveyResult'
+import MockDate from 'mockdate'
+
+interface ISutType {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: ISaveSurveyResultRepository
+}
+
+const makeFakeAddSurveyResult = (): IAddSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_anser',
+  date: new Date()
+})
+const makeFakeSurveyResultModel = (): ISurveyResultModel => ({
+  id: 'any_id',
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_anser',
+  date: new Date()
+})
+
+const makeSaveSurveyResultRepositoryStub = (): ISaveSurveyResultRepository => {
+  class SaveSurveyRepositoryStub implements ISaveSurveyResultRepository {
+    async save (data: IAddSurveyResultModel): Promise<ISurveyResultModel> {
+      return makeFakeSurveyResultModel()
+    }
+  }
+  return new SaveSurveyRepositoryStub()
+}
+
+const makeSut = (): ISutType => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepositoryStub()
+  const sut = new DbSaveSurveyResult({ saveSurveyResultRepository: saveSurveyResultRepositoryStub })
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  }
+}
+
+describe('DbSaveSurveyResult useCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
+  test('should call SaveSurveyRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const repositorySpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    await sut.save(makeFakeAddSurveyResult())
+    expect(repositorySpy).toHaveBeenCalledWith(makeFakeAddSurveyResult())
+  })
+})

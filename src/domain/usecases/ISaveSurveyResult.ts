854766b3c61f020246b65c3144b37250ca0f86a5
Felipe Souza <felipedb91@gmail.com>
2020-11-23 21:25:42 -0300

    feat: add saveSurveyResult useCase

---
+++ src/domain/usecases/ISaveSurveyResult.ts
@@ -0,0 +1,7 @@
+import { ISurveyResultModel } from '../models/ISurveyResultModel'
+
+export type IAddSurveyResultModel = Omit<ISurveyResultModel, 'id'>
+
+export interface ISaveSurveyResult {
+  save: (data: IAddSurveyResultModel) => Promise<ISurveyResultModel>
+}

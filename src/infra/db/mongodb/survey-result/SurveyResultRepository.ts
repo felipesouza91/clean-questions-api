45900fa32ace0f4a7f07d7b12556cf4505c0639a
Felipe Souza <felipedb91@gmail.com>
2020-11-24 22:43:57 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its news

---
+++ src/infra/db/mongodb/survey-result/SurveyResultRepository.ts
@@ -0,0 +1,20 @@
+
+import { ISaveSurveyResultRepository } from '@src/data/protocols/db/survey/ISaveSurveyResultRepository'
+import { ISurveyResultModel } from '@src/domain/models/ISurveyResultModel'
+import { IAddSurveyResultModel } from '@src/domain/usecases/ISaveSurveyResult'
+import { MongoHelper } from '../helpers/MongoHelper'
+
+export class SurveyResultMongoRepository implements ISaveSurveyResultRepository {
+  async save ({ accountId, surveyId, answer, date }: IAddSurveyResultModel): Promise<ISurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const { value: surveyResult } = await surveyResultCollection.findOneAndUpdate({
+      accountId,surveyId
+    }, {
+      $set: { answer, date }
+    },{
+      upsert: true,
+      returnOriginal: false
+    })
+    return surveyResult && MongoHelper.map(surveyResult)
+  }
+}

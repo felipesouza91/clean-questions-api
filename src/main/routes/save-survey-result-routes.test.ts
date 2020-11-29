4d2d7c90a63776eefd5bbc2694a2b2fae7d7a189
Felipe Souza <felipedb91@gmail.com>
2020-11-28 21:01:01 -0300

    test: ensure SaveSurveyResult route return 200 on sucess

--- src/main/routes/save-survey-result-routes.test.ts
+++ src/main/routes/save-survey-result-routes.test.ts
@@ -3,10 +3,43 @@ import request from 'supertest'
 
 import { MongoHelper } from '@src/infra/db/mongodb/helpers/MongoHelper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+import { ISurveyModel } from '@src/domain/models/ISurveyModel'
 
 let surveyCollection: Collection
 let accountCollection: Collection
 let saveSurveyResultCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const account = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@email.com',
+    password: 'any_password',
+    role: 'admin'
+
+  })
+  const id = account.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+  return accessToken
+}
+
+const makeSurvey = async (): Promise<ISurveyModel> => {
+  const survey = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      },
+      {
+        answer: 'other_answer'
+      }
+    ]
+  })
+  return survey.ops[0] && MongoHelper.map(survey.ops[0])
+}
 
 describe('Save Survey Result Routes', () => {
   beforeAll(async () => {
@@ -33,6 +66,17 @@ describe('Save Survey Result Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+    test('Should return 200 on save survey result success', async () => {
+      const survey = await makeSurvey()
+      const accessToken = await makeAccessToken()
+      await request(app)
+        .put(`/api/surveys/${survey.id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200)
     })
   })
 })

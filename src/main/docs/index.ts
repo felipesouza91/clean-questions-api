0e6a032f090041a00fa3df13372c74f1772afc08
Felipe Souza <felipedb91@gmail.com>
2020-12-02 21:06:08 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,12 +1,17 @@
-import { loginPath } from './paths/loginPaths'
+import { loginPath } from './paths'
 import { accountSchema, loginSchema , errorSchema } from './schemas'
 import { badRequest, serverError, unauthorized, notFound } from './components'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node Api',
     description: 'Api do curso do mango para realizar enquetes entre programadores',
     version: '2.2.0'
+  },
+  license: {
+    name: 'GPL 3.0',
+    url: 'https://choosealicense.com/licenses/gpl-3.0/'
   },
   servers: [
     {

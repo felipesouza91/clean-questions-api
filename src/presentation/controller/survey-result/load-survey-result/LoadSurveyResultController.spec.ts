0733172f151bf330d9aeb45db47b9f21513c41e3
Felipe Souza <felipedb91@gmail.com>
2020-12-06 19:47:24 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyResult with correct values

--- src/presentation/controller/survey-result/load-survey-result/LoadSurveyResultController.spec.ts
+++ src/presentation/controller/survey-result/load-survey-result/LoadSurveyResultController.spec.ts
@@ -1,4 +1,4 @@
-import { mockFakeSurvey } from '@src/domain/test'
+import { mockFakeSurvey, mockFakeSurveyResultModel } from '@src/domain/test'
 
 import {
   ILoadSurveyById,
@@ -6,19 +6,23 @@ import {
   forbidden,
   serverError,
   ISurveyModel,
-  InvalidParamError
+  InvalidParamError,
+  ILoadSurveyResult,
+  ISurveyResultModel
 } from './LoadSurveyResultController.protocols'
 import { LoadSurveyResultController } from './LoadSurveyResultController'
 
 interface ISutType {
   sut: LoadSurveyResultController
   loadSurveyByIdStub: ILoadSurveyById
+  loadSurveyResultStub: ILoadSurveyResult
 }
 
 const mockFakeHttpRequest = (): IHttpRequest => ({
   params: {
     surveyId: 'any_survey_id'
-  }
+  },
+  accountId: 'any_account_id'
 })
 
 const mockLoadSurveyByIdStub = (): ILoadSurveyById => {
@@ -30,13 +34,24 @@ const mockLoadSurveyByIdStub = (): ILoadSurveyById => {
   return new DbLoadSurveyByIdStub()
 }
 
+const mockLoadSurveyResultStub = (): ILoadSurveyResult => {
+  class LoadSurveyResultStub implements ILoadSurveyResult {
+    async load (surveyId: string, accountId: string): Promise<ISurveyResultModel> {
+      return mockFakeSurveyResultModel()
+    }
+  }
+  return new LoadSurveyResultStub()
+}
+
 const makeSut = (): ISutType => {
   const loadSurveyByIdStub = mockLoadSurveyByIdStub()
+  const loadSurveyResultStub = mockLoadSurveyResultStub()
   const sut = new LoadSurveyResultController({
-    loadSurveyById: loadSurveyByIdStub
+    loadSurveyById: loadSurveyByIdStub,
+    loadSurveyResult: loadSurveyResultStub
   })
   return {
-    sut, loadSurveyByIdStub
+    sut, loadSurveyByIdStub, loadSurveyResultStub
   }
 }
 
@@ -46,6 +61,12 @@ describe('LoadSurveyResultController', () => {
     const loadSurveyByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
     await sut.handle(mockFakeHttpRequest())
     expect(loadSurveyByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+  test('should call loadSurveyResult with correct values', async () => {
+    const { sut, loadSurveyResultStub } = makeSut()
+    const loadSurveyResutlSpy = jest.spyOn(loadSurveyResultStub, 'load')
+    await sut.handle(mockFakeHttpRequest())
+    expect(loadSurveyResutlSpy).toHaveBeenCalledWith('any_survey_id', 'any_account_id')
   })
   test('should return 403 if loadSurveyById return null', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
